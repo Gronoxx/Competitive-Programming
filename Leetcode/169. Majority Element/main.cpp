e5dc40cc3321826dd36dfe3476315cfff04458f4
Gronoxx <100954782+Gronoxx@users.noreply.github.com>
2024-08-14 17:30:57 -0300

    Create main.cpp

---
+++ Leetcode/169. Majority Element/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for (const auto& pair : m) {
+        if(pair.second > (nums.size()/2)){
+            return pair.first;
+        }
+        }
+        return -1;
+    }
+};
