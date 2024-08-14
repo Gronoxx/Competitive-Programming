dbc968f5fc6067acdce566692de7a575d24afde7
Gronoxx <100954782+Gronoxx@users.noreply.github.com>
2024-08-14 17:28:37 -0300

    Update main.cpp

--- Leetcode/88. Merge Sorted Array/main.cpp
+++ Leetcode/88. Merge Sorted Array/main.cpp
@@ -1,71 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(n>0){
-        int count2=0;
-        queue <int> q;
-        for(int i=0;i<nums1.size();i++){
-            //fila vazia
-            if(q.empty()){
-    
-            if(i>=m){
-            // ja passamos por todos de nums1
-            nums1[i] = nums2[count2];
-            count2++;
-            }
-            else if(nums1[i]<nums2[count2]){}
-            else{
-                q.push(nums1[i]);
-                nums1[i] = nums2[count2];
-                count2++;
-            }
-            
-            }
-            else{
-                if(i>=m && count2>=n){
-                    //so a fila eh valida
-                    nums1[i]=q.front();
-                    q.pop();
-                }
-                else if(i>=m && count2<n){
-                    //fila e count2 valida
-                    if(q.front()<nums2[count2]){
-                        nums1[i]=q.front();
-                        q.pop();
-                    }
-                    else{
-                        nums1[i] = nums2[count2];
-                        count2++;
-                    }
-                }
-                else if(i<m && count2>=n){
-                    //fila e nums1 valido
-                    if(q.front()<nums1[i]){
-                        q.push(nums1[i]);
-                        nums1[i]=q.front();
-                        q.pop();
-                    }
-                    else{
-                    }
-                }
-                //todos validos
-                else if(q.front()<=nums1[i] && q.front() <= nums2[count2]){
-                    q.push(nums1[i]);
-                    nums1[i]=q.front();
-                    q.pop();
-                }
-                else if(nums1[i]<=q.front() &&  nums1[i] <= nums2[count2]){
-                    
-                }
-                else{
-                    q.push(nums1[i]);
-                    nums1[i] = nums2[count2];
-                    count2++;
-                }
+        int i = m - 1;
+        int j = n - 1; 
+        int k = m + n - 1; 
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
             }
         }
-    
-        }        
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 
 };
