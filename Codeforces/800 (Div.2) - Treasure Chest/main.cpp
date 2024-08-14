eb4833e9664725b759043fae1f389691355a3dac
Gronoxx <100954782+Gronoxx@users.noreply.github.com>
2024-08-14 16:10:05 -0300

    Create main.cpp

---
+++ Codeforces/800 (Div.2) - Treasure Chest/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+int main(){
+    int testcases;
+    cin>>testcases;
+    for (int t=0;t<testcases;t++){
+        int c,k,t3;
+        cin>>c;
+        cin>>k;
+        cin>>t3;
+        if(c>k)
+        cout<<c<<endl;
+        else if(c<k){
+        if(c+t3>k)
+        cout<<k<<endl;
+        else
+        cout<<(k+(k-(c+t3)))<<endl;
+        }
+        else
+        cout<<c;
+    }
+    return 0;
+}
